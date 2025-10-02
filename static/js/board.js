4d3dc01e703e419623327bbf7de8dd17ef3a3da7
Haris Mehuljic <haris.mehuljic@drake.edu>
2025-10-02 18:59:47 -0500

    Add ability to flag apples

--- static/js/board.js
+++ static/js/board.js
@@ -1,9 +1,135 @@
 import { Direction } from "./utils.js";
 import { Snake } from "./snake.js";
 
+class BoardTile {
+  constructor(element, positionX, positionY, hasApple = false) {
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.element = element;
+    this.hasApple = hasApple;
+    this.flagged = false;
+
+    // TODO: Remove this debug class
+    if (this.hasApple) {
+      this.element.classList.add("debug-apple");
+    }
+  }
+
+  removeApple() {
+    this.hasApple = false;
+  }
+
+  placeSnake() {
+    this.element.classList.add("snake-body");
+  }
+
+  removeSnake() {
+    this.element.classList.remove("snake-body");
+  }
+
+  placeFlag() {
+    this.element.classList.add("flagged-apple");
+    this.element.classList.remove("debug-apple");
+    this.flagged = true;
+  }
+
+  removeFlag() {
+    this.element.classList.remove("flagged-apple");
+    if (this.hasApple) {
+      this.element.classList.add("debug-apple");
+    }
+    this.flagged = false;
+  }
+}
+
+function findOrphanedApple(appleTiles, allTiles) {
+  let bestApples = [];
+  let bestAppleScore = Infinity;
+
+  appleTiles.forEach((appleTile) => {
+    const appleX = appleTile.positionX;
+    const appleY = appleTile.positionY;
+
+    const relativeCoordinates = [
+      [-1, -1],
+      [0, -1],
+      [1, -1],
+      [-1, 0],
+      [1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+    ];
+
+    let surroundingAppleCount = 0;
+
+    for (const coord of relativeCoordinates) {
+      const checkX = appleX + coord[0];
+      const checkY = appleY + coord[1];
+
+      if (checkX < 0 || checkY < 0) {
+        continue;
+      }
+      if (checkY >= allTiles.length || checkX >= allTiles[0].length) {
+        continue;
+      }
+
+      const checkTile = allTiles[checkY][checkX];
+      if (checkTile.hasApple) {
+        surroundingAppleCount += 1;
+      }
+    }
+
+    if (surroundingAppleCount < bestAppleScore) {
+      bestApples = [appleTile];
+      bestAppleScore = surroundingAppleCount;
+    } else if (surroundingAppleCount === bestAppleScore) {
+      bestApples.push(appleTile);
+    }
+  });
+
+  console.log(`Orphaned apples found: ${bestApples.length}`);
+  const randomIndex = Math.floor(Math.random() * bestApples.length);
+  return bestApples[randomIndex];
+}
+
+function defineBoardTiles() {
+  const tiles = [];
+  const appleTiles = [];
+
+  const appleChance = 0.15;
+  let appleCount = 0;
+
+  document.querySelectorAll(".board-row").forEach((rowElement, y) => {
+    rowElement.querySelectorAll(".board-tile").forEach((tileElement, x) => {
+      if (tiles[y] === undefined) {
+        tiles[y] = [];
+      }
+
+      const hasApple = Math.random() < appleChance;
+      const boardTile = new BoardTile(tileElement, x, y, hasApple);
+
+      if (hasApple) {
+        appleCount += 1;
+        appleTiles.push(boardTile);
+      }
+
+      tiles[y].push(boardTile);
+    });
+  });
+
+  const startingApple = findOrphanedApple(appleTiles, tiles);
+  startingApple.placeFlag();
+
+  return { tiles: tiles, startingApple: startingApple };
+}
+
 export class Board {
   constructor() {
-    this.boardTiles = this.selectBoardElements();
+    const { tiles, startingApple } = defineBoardTiles();
+
+    this.boardTiles = tiles;
+    this.flaggedTiles = [];
     this.currentDirection = Direction.UP;
     this.gameOver = false;
 
@@ -16,23 +142,10 @@ export class Board {
       this.boardTiles.length,
       6
     );
+
+    this.placeFlag(startingApple);
     this.renderSnake();
-  }
-
-  selectBoardElements() {
-    const elements = [];
-
-    document.querySelectorAll(".board-row").forEach((rowElement, y) => {
-      rowElement.querySelectorAll(".board-tile").forEach((tileElement) => {
-        if (elements[y] === undefined) {
-          elements[y] = [];
-        }
-
-        elements[y].push(tileElement);
-      });
-    });
-
-    return elements;
+    this.renderFlags();
   }
 
   checkSnakeCollision() {
@@ -50,7 +163,13 @@ export class Board {
 
       const tileElement = this.boardTiles[y][x];
 
-      tileElement.classList.add("snake-body");
+      tileElement.placeSnake();
+    }
+  }
+
+  renderFlags() {
+    for (const flagTile of this.flaggedTiles) {
+      flagTile.placeFlag();
     }
   }
 
@@ -61,15 +180,19 @@ export class Board {
     const tailX = oldTail.positionX;
 
     this.gameOver = this.checkSnakeCollision();
-    console.log("Game Over:", this.gameOver);
     if (this.gameOver) {
       return;
     }
 
     const tailTileElement = this.boardTiles[tailY][tailX];
-    tailTileElement.classList.remove("snake-body");
+    tailTileElement.removeSnake();
 
     this.renderSnake();
+  }
+
+  placeFlag(tile) {
+    this.flaggedTiles.push(tile);
+    this.renderFlags();
   }
 
   changeDirection(newDirection) {
